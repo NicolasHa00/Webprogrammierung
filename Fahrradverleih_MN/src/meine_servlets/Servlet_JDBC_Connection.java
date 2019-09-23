e8bd02ae3151306e92a770f144d48a4946032dee
Nicolas Hautschek <nicolas.hautschek@sap.com>
2019-09-23 18:27:11 +0200

    Methoden hinzufügen

--- Fahrradverleih_MN/src/meine_servlets/Servlet_JDBC_Connection.java
+++ Fahrradverleih_MN/src/meine_servlets/Servlet_JDBC_Connection.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.ResultSet;
@@ -25,6 +26,7 @@ public class Servlet_JDBC_Connection extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	Connection conn;
+	String connectionURL;
 
 	public Servlet_JDBC_Connection() {
 		super();
@@ -34,15 +36,44 @@ public class Servlet_JDBC_Connection extends HttpServlet {
 	public boolean getMySQLConnection(String hostName, String dbName, String userName, String password) {
 
 		try {
-			String connectionURL = "jdbc:mysql://" + hostName + "/" + dbName + "?user=" + userName + "&password="
-					+ password;
+			connectionURL = "jdbc:mysql://" + hostName + "/" + dbName + "?user=" + userName + "&password=" + password;
 			conn = DriverManager.getConnection(connectionURL);
-			System.out.println("Connection Successful!");
+			System.out.println("Verbindung erfolgreich!");
 			return true;
 
 		} catch (SQLException ex) {
-			System.out.println("Connection not Successful!");
+			System.out.println("Verbindung nicht erfolgreich!");
 			return false;
+		}
+	}
+
+	public void deleteRow(String name) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(connectionURL);
+			PreparedStatement st = conn.prepareStatement("DELETE FROM Table WHERE name = ?");
+			st.setString(1, name);
+			st.executeUpdate();
+			conn.commit();
+			System.out.println("Zeile erfolgreich gelöscht!");
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println("Exception ist aufgetreten!");
+			System.out.println(e.getMessage());
+		}
+	}
+
+	public void addRow(String name) {
+		try {
+			conn = DriverManager.getConnection(connectionURL);
+			Statement st = conn.createStatement();
+			st.executeUpdate("INSERT INTO Customers " + "VALUES (1001, 'Simpson', 'Mr.', 'Springfield', 2001)");
+			conn.close();
+
+		} catch (Exception e) {
+			System.out.println("Exception ist aufgetreten!");
+			System.out.println(e.getMessage());
 		}
 	}
 
