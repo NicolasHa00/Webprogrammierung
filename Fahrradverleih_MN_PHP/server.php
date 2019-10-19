a2f357f4a23d83c2ffa8b1cd6775552150817a3d
Nicolas Hautschek <nicolas.hautschek@sap.com>
2019-10-19 22:29:38 +0200

    fix

--- Fahrradverleih_MN_PHP/server.php
+++ Fahrradverleih_MN_PHP/server.php
@@ -5,40 +5,35 @@ $username = "";
 $email    = "";
 $errors = array();
 
-// connect to database
+// Mit der Datenbank verbinden
 $db = mysqli_connect('localhost', 'root', '', 'fahrradverleih_mn');
 
-// REGISTER
+// Registrieren
 
 if (isset($_POST['reg_user'])) {
-    // receive all input values from the form
     $username = mysqli_real_escape_string($db, $_POST['username']);
     $email = mysqli_real_escape_string($db, $_POST['email']);
     $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
     $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
     
-    // form validation: ensure that the form is correctly filled 
-    // by adding (array_push()) corresponding error unto $errors array
-    if (empty($username)) { array_push($errors, "Username is required"); }
-    if (empty($email)) { array_push($errors, "Email is required"); }
-    if (empty($password_1)) { array_push($errors, "Password is required"); }
+    if (empty($username)) { array_push($errors, "Benutzername wird benötigt!"); }
+    if (empty($email)) { array_push($errors, "E-Mail wird benötigt!"); }
+    if (empty($password_1)) { array_push($errors, "Password wird benötigt!"); }
     if ($password_1 != $password_2) {
-        array_push($errors, "The two passwords do not match");
+        array_push($errors, "Die beiden Passwörter unterscheiden sich!");
     }
     
-    // first check the database to make sure
-    // a user does not already exist with the same username and/or email
     $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
     
-    if ($user) { // if user exists
+    if ($user) { 
         if ($user['username'] === $username) {
-            array_push($errors, "Username already exists");
+            array_push($errors, "Benutzername existiert bereits!");
         }
         
         if ($user['email'] === $email) {
-            array_push($errors, "email already exists");
+            array_push($errors, "E-Mail existiert bereits!");
         }
     }
     
@@ -51,7 +46,7 @@ if (isset($_POST['reg_user'])) {
   			  VALUES('$username', '$email', '$password')";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
-        $_SESSION['success'] = "You are now logged in";
+        $_SESSION['success'] = "Sie haben sich erfolgreich eingeloggt!";
         header('location: index.php');
     }
 }
@@ -63,10 +58,10 @@ if (isset($_POST['login_user'])) {
     $password = mysqli_real_escape_string($db, $_POST['password']);
     
     if (empty($username)) {
-        array_push($errors, "Username is required");
+        array_push($errors, "Benutzername wird benötigt!");
     }
     if (empty($password)) {
-        array_push($errors, "Password is required");
+        array_push($errors, "Passwort wird benötigt!");
     }
     
     if (count($errors) == 0) {
@@ -75,12 +70,12 @@ if (isset($_POST['login_user'])) {
         $results = mysqli_query($db, $query);
         if (mysqli_num_rows($results) == 1) {
             $_SESSION['username'] = $username;
-            $_SESSION['success'] = "You are now logged in";
+            $_SESSION['success'] = "Sie haben sich erfolgreich eingeloggt!";
             header('location: index.php');
             setcookie("UsernameCookie", $username, time()+86400);
         }
         else {
-            array_push($errors, "Wrong username/password combination");
+            array_push($errors, "Falsche Benutzername/Passwort Kombination!");
         }
     }
 }
