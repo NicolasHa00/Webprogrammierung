3af405f3d211cc266099648693eb36419f88becf
Nicolas Hautschek <nicolas.hautschek@sap.com>
2019-10-19 22:22:04 +0200

    fix

--- Fahrradverleih_MN_PHP/index.php
+++ Fahrradverleih_MN_PHP/index.php
@@ -2,7 +2,7 @@
 session_start();
 
 if (! isset($_SESSION['username'])) {
-    $_SESSION['msg'] = "You must log in first";
+    $_SESSION['msg'] = "Sie müssen sich zuerst einloggen!";
     header('location: login.php');
 }
 if (isset($_GET['logout'])) {
